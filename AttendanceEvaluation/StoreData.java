8e6a5c400234828d8a3a05f38084e842d5286799
Rahil Shaban <rahilshaban07@gmail.com>
2020-03-15 18:16:03 +0530

    18:15 new features added. Last date not working properly

--- AttendanceEvaluation/StoreData.java
+++ AttendanceEvaluation/StoreData.java
@@ -7,8 +7,9 @@ import java.io.*;
  * This class inputs date and absentee roll nos and writes the data in the file "absenteeList.txt" found in main package.
  * Path: D:\\BlueJ Stuff\\Rahil XI\\AbsenteeList.txt
  *
- *How to read the file (for humans): Sample: 15 3 2020   5   2   5   16  23  30
- *The first 3 numbers is the date. The fourth is the number of absentees (can be 0). The remaining are the roll nos of the absentees (There can be no absentees too).
+ *How to read the file (for humans): Sample: 15 3 2020  1   5      2   5   16  23  30
+ *The first 3 numbers is the date. The fourth is the serial Number, to be used by the software primarily.The fifth is the number of absentees (can be 0).
+ *The remaining are the roll nos of the absentees (There can be no absentees too).
  *
  * @author BoltonB07
  * @version 1.0.0 15/3/2020
@@ -20,16 +21,35 @@ public class StoreData
         FileWriter file = new FileWriter("absenteeList.txt",true);
         BufferedWriter bw=new BufferedWriter(file);
         String date="",absenteeList="";
-
         boolean continueLoop=true;
-        int iterationCounter=0;
+        int iterationCounter=0, serialNum=0;
+        File f=new File("absenteeList.txt");
+
+        if(f.length()==0){
+            serialNum=0; System.out.println("File is currently empty");
+        }
+        else{
+            Scanner line;
+            FileReader fileLastLine=new FileReader("AbsenteeList.txt");
+            BufferedReader lastLine=new BufferedReader(fileLastLine);
+            String lastLineOfData=null;
+            while((lastLineOfData = lastLine.readLine()) != null){
+                serialNum++;
+            }
+            line=new Scanner(lastLineOfData);
+            serialNum++;
+            System.out.println("The last entered date was: "+line.next()+" "+line.next()+" "+line.next());
+        }
         while(continueLoop){
             System.out.println('\u000C');  //Clears screen (The previous iteration)
-            //Input part of this program
-            if(iterationCounter>0){   //QOL (Quality of Life)
-                System.out.println("Number of entered days (in the current session): "+iterationCounter);
-                System.out.println("Last entered date was: "+date);
+
+            if(iterationCounter>0){
+                System.out.println("Number of entered days (IN THE CURRENT SESSION): "+iterationCounter);
+                System.out.println("The last entered date was: "+date);
+
             }
+
+            System.out.println("Lines of data already entered :"+serialNum);
             System.out.println("Enter a date (<date in int> <month in int> <year in int>) Example = 17 3 2020");
             System.out.println("Enter \"exitprog\" anytime to terminate the program without entering the current cycle's data");
             date=br.readLine();
@@ -43,13 +63,14 @@ public class StoreData
                 continueLoop=false;
                 break;
             }
+            serialNum++;
 
             //Writing part of this program
             {
+
                 StringTokenizer tk=new StringTokenizer(absenteeList,",");
                 //writing to file
-                bw.write(date);
-                bw.write("\t"+tk.countTokens());
+                bw.write(date+"\t"+serialNum+"\t"+tk.countTokens()+"\t");
                 while(tk.hasMoreTokens()){
                     bw.write("\t"+tk.nextToken());
                 }
