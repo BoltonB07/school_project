349b8b296ad777bdd0b97e560d7ca35dbddb4bbb
BinoManjesh <binomanjesh@gmil.com>
2020-05-04 22:06:30 +0530

    Replaced plotting of time values with moving averages

--- com/rahil/SortGraph/SortingGraph.java
+++ com/rahil/SortGraph/SortingGraph.java
@@ -10,9 +10,6 @@ class SortingGraph extends JPanel {
 
     private static final int WIDTH = 640, HEIGHT = 500;
 
-    //Change the number of sorting algorithms if you add more
-    private static final int SORTING_ALGORITHMS = 6;
-
     private static final SortingThread.SortingFunction[] SORTING_FUNCTIONS = new SortingThread.SortingFunction[]{
             Arrays :: sort,
             Sort :: Bubble,
@@ -20,7 +17,7 @@ class SortingGraph extends JPanel {
             Sort :: Selection,
             Sort :: Merge,
             Sort :: Quick
-            //Add method references to sorting algorithms in the format <Class name> :: method name
+            //Add method references to sorting algorithms in the format <Class name> :: <method name>
     };
 
     //Add names of the algorithms in the same order as above
@@ -31,7 +28,8 @@ class SortingGraph extends JPanel {
     private static final Color[] COLORS = new Color[]{Color.BLACK, Color.RED, Color.GREEN, Color.BLUE, Color.YELLOW, Color.ORANGE};
 
     JFrame frame;
-    double[][] lines;
+    double[][] times;
+    double [][] movingAverages;
 
     SortingGraph() {
         super();
@@ -44,7 +42,8 @@ class SortingGraph extends JPanel {
 
     public static void main(String[] args) {
         SortingThread.graph = new SortingGraph();
-        SortingThread.graph.lines = new double[SORTING_ALGORITHMS][MAX_ARRAY_SIZE];
+        SortingThread.graph.times = new double[SORTING_FUNCTIONS.length][MAX_ARRAY_SIZE];
+        SortingThread.graph.movingAverages = new double[SORTING_FUNCTIONS.length][MAX_ARRAY_SIZE];
         SortingThread.randomArrays = new int[MAX_ARRAY_SIZE][];
         for (int i = 0; i < MAX_ARRAY_SIZE; i++) {
             SortingThread.randomArrays[i] = new int[i + 1];
@@ -54,10 +53,11 @@ class SortingGraph extends JPanel {
         }
         System.out.format("%s: Finished making %d random arrays.%n", Thread.currentThread().getName(), MAX_ARRAY_SIZE);
         SwingUtilities.invokeLater(() -> SortingThread.graph.frame.setVisible(true));
-        SortingThread[] sortingThreads = new SortingThread[SORTING_ALGORITHMS];
-        for (int i = 0; i < SORTING_ALGORITHMS; i++) {
+        SortingThread[] sortingThreads = new SortingThread[SORTING_FUNCTIONS.length];
+        for (int i = 0; i < SORTING_FUNCTIONS.length; i++) {
             sortingThreads[i] = new SortingThread(SORTING_FUNCTIONS[i], NAMES[i]);
-            sortingThreads[i].line = SortingThread.graph.lines[i];
+            sortingThreads[i].times = SortingThread.graph.times[i];
+            sortingThreads[i].movingAverages = SortingThread.graph.movingAverages[i];
         }
         for (SortingThread thread : sortingThreads) {
             thread.start();
@@ -69,12 +69,12 @@ class SortingGraph extends JPanel {
     synchronized public void paintComponent(Graphics g) {
         super.paintComponent(g);
         final double xScale = (double) WIDTH / MAX_ARRAY_SIZE, yScale = HEIGHT / 1e-3;
-        for (int i = 0; i < SORTING_ALGORITHMS; ++i) {
+        for (int i = 0; i < SORTING_FUNCTIONS.length; ++i) {
             g.setColor(COLORS[i]);
-            int prevY = (int) (lines[i][0] * yScale);
+            int prevY = (int) (times[i][0] * yScale);
             int prevX = 0;
             for (int j = 1; j < MAX_ARRAY_SIZE; j++) {
-                int thisY = (int) (lines[i][j] * yScale);
+                int thisY = (int) (movingAverages[i][j] * yScale);
                 int thisX = (int) (xScale * j);
                 g.drawLine(prevX, HEIGHT - prevY, thisX, HEIGHT - thisY);
                 prevX = thisX;
--- com/rahil/SortGraph/SortingThread.java
+++ com/rahil/SortGraph/SortingThread.java
@@ -4,11 +4,14 @@ import java.util.Arrays;
 
 class SortingThread extends Thread {
 
+    private static final int MOVING_AVERAGE_SIZE = 101;
+
     static int[][] randomArrays;
     static SortingGraph graph;
 
     SortingFunction sorter;
-    double[] line;
+    double[] times;
+    double[] movingAverages;
 
     SortingThread(SortingFunction sorter, String name) {
         super(name);
@@ -17,18 +20,29 @@ class SortingThread extends Thread {
 
     @Override
     public void run() {
-        for (int i = 0; i < SortingGraph.MAX_ARRAY_SIZE; i++) {
+        for (int i = 0; i < times.length; i++) {
             int[] arr = Arrays.copyOf(randomArrays[i], randomArrays[i].length);
             long startTime = System.nanoTime();
             sorter.sort(arr);
             double elapsedTime = (System.nanoTime() - startTime) / 1e9;
-            line[i] = elapsedTime;
+            times[i] = elapsedTime;
+            if (i == MOVING_AVERAGE_SIZE - 1) {
+                for (int j = 0; j < MOVING_AVERAGE_SIZE; j++) {
+                    movingAverages[i / 2] += times[j];
+                }
+                movingAverages[i / 2] /= MOVING_AVERAGE_SIZE;
+            } else if (i > MOVING_AVERAGE_SIZE - 1) {
+                int j = i - MOVING_AVERAGE_SIZE / 2;
+                movingAverages[j] = movingAverages[j - 1] * MOVING_AVERAGE_SIZE + times[i] - times[i - MOVING_AVERAGE_SIZE];
+                movingAverages[j] /= MOVING_AVERAGE_SIZE;
+            }
             graph.repaint();
         }
         System.out.format("%s: I finished!%n", Thread.currentThread().getName());
     }
 
     interface SortingFunction {
+
         void sort(int[] arr);
     }
 }
