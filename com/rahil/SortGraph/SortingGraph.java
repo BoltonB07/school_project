5f9e60ca411181215c1525f4868f57589c6972c0
BinoManjesh <binomanjesh@gmil.com>
2020-05-05 22:36:55 +0530

    Added the drawGraph function

--- com/rahil/SortGraph/SortingGraph.java
+++ com/rahil/SortGraph/SortingGraph.java
@@ -8,7 +8,8 @@ class SortingGraph extends JPanel {
 
     static final int MAX_ARRAY_SIZE = 10_000;
 
-    private static final int WIDTH = 640, HEIGHT = 500;
+    private static final int GRAPH_WIDTH = 640, GRAPH_HEIGHT = 500;
+    private static final double X_SCALE = (double) GRAPH_WIDTH / MAX_ARRAY_SIZE, Y_SCALE = GRAPH_HEIGHT / 1e-3;
 
     private static final SortingThread.SortingFunction[] SORTING_FUNCTIONS = new SortingThread.SortingFunction[]{
             Arrays :: sort,
@@ -25,15 +26,16 @@ class SortingGraph extends JPanel {
             , "Merge sort", "Quick sort"};
 
     //Add colors of the graph for each algorithm in the same order as above
-    private static final Color[] COLORS = new Color[]{Color.BLACK, Color.RED, Color.GREEN, Color.BLUE, Color.YELLOW, Color.ORANGE};
+    private static final Color[] COLORS = new Color[]{Color.BLACK, Color.RED, Color.GREEN, Color.BLUE, Color.YELLOW,
+            Color.ORANGE};
 
     JFrame frame;
     double[][] times;
-    double [][] movingAverages;
+    double[][] movingAverages;
 
     SortingGraph() {
         super();
-        setPreferredSize(new Dimension(WIDTH, HEIGHT));
+        setPreferredSize(new Dimension(GRAPH_WIDTH, GRAPH_HEIGHT));
         frame = new JFrame();
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.add(this);
@@ -68,15 +70,21 @@ class SortingGraph extends JPanel {
     @Override
     synchronized public void paintComponent(Graphics g) {
         super.paintComponent(g);
-        final double xScale = (double) WIDTH / MAX_ARRAY_SIZE, yScale = HEIGHT / 1e-3;
-        for (int i = 0; i < SORTING_FUNCTIONS.length; ++i) {
+        drawGraph(0, 0, movingAverages, g);
+    }
+
+    void drawGraph(int x, int y, double[][] mat, Graphics g) {
+        for (int i = 0; i < mat.length; ++i) {
             g.setColor(COLORS[i]);
-            int prevY = (int) (times[i][0] * yScale);
+            int prevY = (int) (mat[i][0] * Y_SCALE);
             int prevX = 0;
             for (int j = 1; j < MAX_ARRAY_SIZE; j++) {
-                int thisY = (int) (movingAverages[i][j] * yScale);
-                int thisX = (int) (xScale * j);
-                g.drawLine(prevX, HEIGHT - prevY, thisX, HEIGHT - thisY);
+                int thisX = x + (int) (X_SCALE * j);
+                int thisY = y + (int) (GRAPH_HEIGHT - mat[i][j] * Y_SCALE);
+                if (thisX < x || thisX > x + GRAPH_WIDTH || thisY < y || thisY > y + GRAPH_HEIGHT) {
+                    break;
+                }
+                g.drawLine(prevX, prevY, thisX, thisY);
                 prevX = thisX;
                 prevY = thisY;
             }
